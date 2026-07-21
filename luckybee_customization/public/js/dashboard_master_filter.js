231a5cbea2d7c68538f3a088332f7bc5fc8d925b
Ebbad <ebbadsiddique@gmail.com>
2026-07-21 15:29:13 +0500

    fix: refactor dashboard master filter using monkey patch

--- luckybee_customization/public/js/dashboard_master_filter.js
+++ luckybee_customization/public/js/dashboard_master_filter.js
@@ -1,18 +1,21 @@
-$(document).on('page-change', function() {
-    let route = frappe.get_route();
-    if (route[0] === 'dashboard-view' && route[1] === 'Sales Analysis') {
-        setTimeout(function() {
-            let view = frappe.container.page.page_view;
-            if (!view || !view.page) return;
+frappe.provide('frappe.views');
 
-            // In Frappe 15/16, Dashboard View hides page_form. We unhide it.
-            if (view.page.page_form) {
-                view.page.page_form.removeClass('hide');
-            }
-
-            // Add custom fields if not already added
-            if (!view.page.fields_dict.master_from_date) {
-                view.page.add_field({
+frappe.ready(function() {
+    if (frappe.views.DashboardView) {
+        let original_setup_page = frappe.views.DashboardView.prototype.setup_page;
+        frappe.views.DashboardView.prototype.setup_page = function() {
+            original_setup_page.call(this);
+            
+            // In DashboardView, this.doctype holds the dashboard name (e.g. 'Sales Analysis')
+            if (this.doctype === 'Sales Analysis') {
+                if (this.page && this.page.page_form) {
+                    this.page.page_form.removeClass('hide');
+                }
+                
+                let view = this;
+                
+                // Add custom fields
+                this.page.add_field({
                     fieldname: 'master_from_date',
                     label: __('From Date'),
                     fieldtype: 'Date',
@@ -21,7 +24,8 @@ $(document).on('page-change', function() {
                         update_dashboard_charts(view);
                     }
                 });
-                view.page.add_field({
+                
+                this.page.add_field({
                     fieldname: 'master_to_date',
                     label: __('To Date'),
                     fieldtype: 'Date',
@@ -30,29 +34,38 @@ $(document).on('page-change', function() {
                         update_dashboard_charts(view);
                     }
                 });
-                
-                // trigger initial fetch with defaults
-                setTimeout(() => update_dashboard_charts(view), 500);
             }
-        }, 200); // Wait for page to initialize
+        };
+
+        let original_render_dashboard_charts = frappe.views.DashboardView.prototype.render_dashboard_charts;
+        frappe.views.DashboardView.prototype.render_dashboard_charts = function() {
+            original_render_dashboard_charts.call(this);
+            
+            if (this.doctype === 'Sales Analysis') {
+                // Trigger initial fetch
+                setTimeout(() => {
+                    update_dashboard_charts(this);
+                }, 500);
+            }
+        };
     }
 });
 
 function update_dashboard_charts(view) {
+    if (!view.page.fields_dict.master_from_date || !view.page.fields_dict.master_to_date) return;
+    
     let from_date = view.page.fields_dict.master_from_date.get_value();
     let to_date = view.page.fields_dict.master_to_date.get_value();
     
     if (from_date && to_date && view.chart_group && view.chart_group.widgets) {
         view.chart_group.widgets.forEach(widget => {
             if (widget.chart_doc && widget.fetch) {
-                // If it's a Report chart, it expects filters.from_date
                 if (widget.chart_doc.chart_type === 'Report') {
                     if (!widget.filters) widget.filters = {};
                     widget.filters.from_date = from_date;
                     widget.filters.to_date = to_date;
                     widget.fetch_and_update_chart();
                 } else {
-                    // For standard charts, we can pass args
                     let args = { from_date: from_date, to_date: to_date };
                     widget.fetch(widget.filters, true, args).then(() => {
                         widget.update_chart_object();
